59c0199d71e459191f6aae4cfbea831c61f1be20
Robert Harrison <harrisonrw@gmail.com>
2025-02-03 10:54:30 -0800

    Initial Xcode Project

---
+++ CustomToolbar/CustomToolbar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomToolbar
+//
+//  Created by Robert Harrison on 2/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomToolbar/CustomToolbar/CustomToolbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomToolbarApp.swift
+//  CustomToolbar
+//
+//  Created by Robert Harrison on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomToolbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
