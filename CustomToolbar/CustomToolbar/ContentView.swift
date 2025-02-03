8d2b6148b8ec79f12daf664adb00b24e865d7248
Robert Harrison <harrisonrw@gmail.com>
2025-02-03 13:51:08 -0800

    Custom Toolbar at the Top

--- CustomToolbar/CustomToolbar/ContentView.swift
+++ CustomToolbar/CustomToolbar/ContentView.swift
@@ -7,15 +7,102 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct SampleView: View {
+    let title: String
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Text(title)
+            .foregroundStyle(.black)
+            .font(.system(size: 24))
+    }
+}
+
+enum NavToolbarItem {
+    case profile
+    case home
+    case cloud
+    case folder
+}
+
+struct ToolbarButton: View {
+    let identifier: NavToolbarItem
+    let image: String
+    @Binding var selectedToolbarItem: NavToolbarItem
+    var namespace: Namespace.ID
+    
+    var body: some View {
+        Button {
+            selectedToolbarItem = identifier
+        } label: {
+            ZStack {
+                if selectedToolbarItem == identifier {
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(Color.blue)
+                        .matchedGeometryEffect(id: "hoverEffect", in: namespace)
+                }
+                
+                Image(systemName: image)
+                    .foregroundStyle(.white)
+                    .font(.system(size: 18))
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 4)
+                
+            }
         }
-        .padding()
+    }
+}
+
+struct ContentView: View {
+    @State private var selectedToolbarItem: NavToolbarItem = .home
+    @Namespace private var animationNamespace
+    
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Color.white
+                    .edgesIgnoringSafeArea(.all)
+                
+                switch selectedToolbarItem {
+                case .profile:
+                    SampleView(title: "Profile")
+                case .home:
+                    SampleView(title: "Home")
+                case .cloud:
+                    SampleView(title: "Cloud")
+                case .folder:
+                    SampleView(title: "Folder")
+                }
+            }
+            .navigationTitle("Item List")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    HStack(spacing: 15) {
+                        ToolbarButton(identifier: .profile,
+                                      image: "person.circle.fill",
+                                      selectedToolbarItem: $selectedToolbarItem,
+                                      namespace: animationNamespace)
+                        
+                        ToolbarButton(identifier: .home,
+                                      image: "house.fill",
+                                      selectedToolbarItem: $selectedToolbarItem,
+                                      namespace: animationNamespace)
+                        
+                        ToolbarButton(identifier: .cloud,
+                                      image: "cloud.fill",
+                                      selectedToolbarItem: $selectedToolbarItem,
+                                      namespace: animationNamespace)
+                        
+                        ToolbarButton(identifier: .folder,
+                                      image: "folder.fill",
+                                      selectedToolbarItem: $selectedToolbarItem,
+                                      namespace: animationNamespace)
+                    }
+                    .background(Color.black.opacity(0.7))
+                    .clipShape(Capsule())
+                }
+            }
+        }
     }
 }
 
